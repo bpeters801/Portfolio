c4cf6614ba9e46ef0bfb1819071ccc649d567b8e
Brittani Peterson <britdoan@gmail.com>
2022-03-23 12:35:26 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// var before = document.getElementById("divToShowHide");
+// var after = document.getElementById("divToShow")
+
+// before.addEventListener = ("scroll", function (){
+//     after.style.animation
+// })
+
+
+// // document.getElementById("square").addEventListener("wheel", function (){
+// //     squareColor.style.backgroundColor = "orange";
+// // })
